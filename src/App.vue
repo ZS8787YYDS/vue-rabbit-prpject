6e2c11256d8314e45a0156f6ac756a292af0ac4a
zishengyyds <zisheng8787@gitee.com>
2023-12-26 00:47:46 +0800

    finished the static structure of the  home page

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <el-button type="success">Primary</el-button>
-  // 一级路由出口
   <router-view></router-view>
-  <div class="cls">
-    睡觉睡觉
-  </div>
 </template>
 
 <style scoped lang="scss">
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,10 +1,14 @@
 <script setup>
-
+import LayoutNav from '@/views/layout/Components/LayoutNav.vue'
+import LayoutHeader from '@/views/layout/Components/LayoutHeader.vue'
+import LayoutFooter from '@/views/layout/Components/LayoutFooter.vue'
 </script>
 <template>
     <div>
-        我是首页
+        <layout-nav></layout-nav>
+        <layout-header></layout-header>
         <router-view></router-view>
+        <layout-footer></layout-footer>
     </div>
 </template>
 <style scoped>

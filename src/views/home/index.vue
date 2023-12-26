9d5397ef9b1d44878a8d2bace281684078acde37
zishengyyds <zisheng8787@gitee.com>
2023-12-26 22:29:30 +0800

    finished the entire structure division of the home page

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,19 @@
+// 入口组件
 <script setup>
-
+import HomeBanner from "./Components/HomeBanner.vue";
+import HomeCategory from "./Components/HomeCategory.vue";
+import HomeFreshVue from "./Components/HomeFresh.vue";
+import HomeProductListVue from "./Components/HomeProductList.vue";
+import HomeRecommendVue from "./Components/HomeRecommend.vue";
 </script>
 <template>
-    <div >
-        我是主页面
+    <div>
+        <Home-category></Home-category>
+        <Home-banner></Home-banner>
     </div>
+    <HomeFreshVue></HomeFreshVue>
+    <HomeRecommendVue></HomeRecommendVue>
+    <HomeProductListVue></HomeProductListVue>
 </template>
 <style scoped>
 
